dc8039acf413d52fde6cc865519b1baacf30fd2b
Ronak Vanpariya <vanpariyar@gmail.com>
2021-06-06 17:41:41 +0530

    Added new file :tada:

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # hari-acf-flexible-fields-addon
 [ Only With ACF Pro ] this is acf pro helper for template based Flexible fields layouts.
+
+## Requirements
+
+1. ACF pro
+2. Flexible fields layout with `sections` fields name.
+3. Create folder in theme `flexible-fields`
+4. Inside that folder put file same as flexible field by replacing `_` with `-`. Means, if field name is `banner_section` your file name willbe `banner-section.php`.
+
+## usage
+- Make field in `section` called `banner_section`.
+- Add field in the `banner_section` like `name`, `button_text` etc.
+- In `banner-section.php` add
+```php
+
+<h1><?php get_sub_field('button_text'); ?> </h1>
+ 
+<button> <?php get_sub_field('button_text'); ?> </button>
+
+?>
+```
+
+### add more fields and enjoy :tada:
--- hari-acf-flexible-fields-addon.php
+++ hari-acf-flexible-fields-addon.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     Hari Acf Flexible Fields Addon
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://github.com/vanpariyar/hari-acf-flexible-fields-addon
+ * Description:     Plugin contains template support for the ACF flexible fields layout
+ * Author:          Ronak Vanpariya
+ * Author URI:      https://vanpariyar.in
  * Text Domain:     hari-acf-flexible-fields-addon
  * Domain Path:     /languages
  * Version:         0.1.0
@@ -13,3 +13,60 @@
  */
 
 // Your code starts here.
+
+
+
+if ( !function_exists( 'add_action' ) ) {
+	echo __('Hi there!  I\'m just a plugin, not much I can do when called directly.', 'useful-custom-rule-for-automatewoo');
+	exit;
+}
+
+/* Plugin Constants */
+if (!defined('Hari_Acf_Flexible_Fields_Addon_URL')) {
+    define('Hari_Acf_Flexible_Fields_Addon_URL', plugin_dir_url(__FILE__));
+}
+
+if (!defined('Hari_Acf_Flexible_Fields_Addon_PLUGIN_PATH')) {
+    define('Hari_Acf_Flexible_Fields_Addon_PLUGIN_PATH', plugin_dir_path(__FILE__));
+}
+
+require_once Hari_Acf_Flexible_Fields_Addon_PLUGIN_PATH.'/includes/PageTemplater.php';
+
+class Hari_Acf_Flexible_Fields_Addon {
+    
+	/**
+	 * A reference to an instance of this class.
+	 */
+	private static $instance;
+
+    public static function get_instance() {
+
+		if ( null == self::$instance ) {
+			self::$instance = new PageTemplater();
+		}
+
+		return self::$instance;
+
+	}
+
+    public function __construct(){
+
+        add_action('plugins_loaded', array( $this,'check_some_other_plugin'), 10 );
+        // PageTemplater::get_instance();
+        add_action( 'plugins_loaded', array( 'PageTemplater', 'get_instance' ) );
+    }
+
+    public function check_some_other_plugin() {
+		if ( ! class_exists( 'acf_pro' ) ) {
+			add_action( 'admin_notices', function() {				
+				echo '<div class="error"><p><strong>' . esc_html__( 'Hari Acf Flexible Fields Addon plugin require Advanced Cutom Field Pro Plugin installed / activated', 'useful-custom-rule-for-automatewoo' ) . '</strong><code>Link:- http://advancedcustomfields.com/pro/</code></p></div>';
+			} );
+			return;
+		}
+    }
+    
+
+
+}
+
+Hari_Acf_Flexible_Fields_Addon::get_instance();
