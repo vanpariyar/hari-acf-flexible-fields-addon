78a5c4c1d4242fd20b12d74d40c5053c19b86892
Ronak Vanpariya <vanpariyar@gmail.com>
2021-06-06 15:56:51 +0530

    initital commit

---
+++ hari-acf-flexible-fields-addon.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Hari Acf Flexible Fields Addon
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     hari-acf-flexible-fields-addon
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Hari_Acf_Flexible_Fields_Addon
+ */
+
+// Your code starts here.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Hari_Acf_Flexible_Fields_Addon
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
